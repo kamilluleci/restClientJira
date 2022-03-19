bb13690f08e2af37493fc1128eaeccc0bbb41eb1
KAMİL LÜLECİ <kamilluleci@gmail.com>
2022-03-19 11:27:02 +0300

    Add project files.

---
+++ restClient/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿
+namespace restClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtRestURI = new System.Windows.Forms.TextBox();
+            this.txtResponse = new System.Windows.Forms.TextBox();
+            this.cmdGO = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.rdoBasic = new System.Windows.Forms.RadioButton();
+            this.rdoNTLMAuth = new System.Windows.Forms.RadioButton();
+            this.rdoNetCred = new System.Windows.Forms.RadioButton();
+            this.rdoRollOwn = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtRestURI
+            // 
+            this.txtRestURI.Location = new System.Drawing.Point(121, 27);
+            this.txtRestURI.Name = "txtRestURI";
+            this.txtRestURI.Size = new System.Drawing.Size(457, 20);
+            this.txtRestURI.TabIndex = 0;
+            // 
+            // txtResponse
+            // 
+            this.txtResponse.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtResponse.Location = new System.Drawing.Point(121, 166);
+            this.txtResponse.Multiline = true;
+            this.txtResponse.Name = "txtResponse";
+            this.txtResponse.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtResponse.Size = new System.Drawing.Size(646, 180);
+            this.txtResponse.TabIndex = 1;
+            // 
+            // cmdGO
+            // 
+            this.cmdGO.Location = new System.Drawing.Point(584, 25);
+            this.cmdGO.Name = "cmdGO";
+            this.cmdGO.Size = new System.Drawing.Size(183, 23);
+            this.cmdGO.TabIndex = 2;
+            this.cmdGO.Text = "GO!";
+            this.cmdGO.UseVisualStyleBackColor = true;
+            this.cmdGO.Click += new System.EventHandler(this.cmdGO_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Request URI:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 169);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Response:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 304);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 42);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Go to Form2";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdoNTLMAuth);
+            this.groupBox1.Controls.Add(this.rdoBasic);
+            this.groupBox1.Enabled = false;
+            this.groupBox1.Location = new System.Drawing.Point(121, 87);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 73);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Auth Type";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.rdoNetCred);
+            this.groupBox2.Controls.Add(this.rdoRollOwn);
+            this.groupBox2.Location = new System.Drawing.Point(396, 87);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(182, 73);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Technique";
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(121, 61);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(200, 20);
+            this.txtUserName.TabIndex = 8;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(396, 61);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(182, 20);
+            this.txtPassword.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(16, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(58, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Username:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(337, 64);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(53, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Password";
+            // 
+            // rdoBasic
+            // 
+            this.rdoBasic.AutoSize = true;
+            this.rdoBasic.Checked = true;
+            this.rdoBasic.Location = new System.Drawing.Point(6, 22);
+            this.rdoBasic.Name = "rdoBasic";
+            this.rdoBasic.Size = new System.Drawing.Size(122, 17);
+            this.rdoBasic.TabIndex = 0;
+            this.rdoBasic.TabStop = true;
+            this.rdoBasic.Text = "Basic Authentication";
+            this.rdoBasic.UseVisualStyleBackColor = true;
+            // 
+            // rdoNTLMAuth
+            // 
+            this.rdoNTLMAuth.AutoSize = true;
+            this.rdoNTLMAuth.Location = new System.Drawing.Point(6, 45);
+            this.rdoNTLMAuth.Name = "rdoNTLMAuth";
+            this.rdoNTLMAuth.Size = new System.Drawing.Size(108, 17);
+            this.rdoNTLMAuth.TabIndex = 1;
+            this.rdoNTLMAuth.Text = "NTLM (Windows)";
+            this.rdoNTLMAuth.UseVisualStyleBackColor = true;
+            // 
+            // rdoNetCred
+            // 
+            this.rdoNetCred.AutoSize = true;
+            this.rdoNetCred.Location = new System.Drawing.Point(6, 46);
+            this.rdoNetCred.Name = "rdoNetCred";
+            this.rdoNetCred.Size = new System.Drawing.Size(140, 17);
+            this.rdoNetCred.TabIndex = 3;
+            this.rdoNetCred.Text = "NetworkCredential Class";
+            this.rdoNetCred.UseVisualStyleBackColor = true;
+            // 
+            // rdoRollOwn
+            // 
+            this.rdoRollOwn.AutoSize = true;
+            this.rdoRollOwn.Checked = true;
+            this.rdoRollOwn.Location = new System.Drawing.Point(6, 23);
+            this.rdoRollOwn.Name = "rdoRollOwn";
+            this.rdoRollOwn.Size = new System.Drawing.Size(93, 17);
+            this.rdoRollOwn.TabIndex = 2;
+            this.rdoRollOwn.TabStop = true;
+            this.rdoRollOwn.Text = "Roll Your Own";
+            this.rdoRollOwn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 382);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdGO);
+            this.Controls.Add(this.txtResponse);
+            this.Controls.Add(this.txtRestURI);
+            this.Name = "Form1";
+            this.Text = "C# REST Client";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtRestURI;
+        private System.Windows.Forms.TextBox txtResponse;
+        private System.Windows.Forms.Button cmdGO;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rdoNTLMAuth;
+        private System.Windows.Forms.RadioButton rdoBasic;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton rdoNetCred;
+        private System.Windows.Forms.RadioButton rdoRollOwn;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ restClient/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace restClient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region UI Event Handlers
+        private void cmdGO_Click(object sender, EventArgs e)
+        {
+            //Test urls (URI)
+            //https://dry-cliffs-19849.herokuapp.com/
+            //https://dry-cliffs-19849.herokuapp.com/users
+            //https://dry-cliffs-19849.herokuapp.com/users.json
+            //https://dry-cliffs-19849.herokuapp.com/users/2.html
+
+            RestClient rClient = new RestClient();
+            rClient.endPoint = txtRestURI.Text;
+
+            if(rdoRollOwn.Checked)
+            {
+                rClient.authTech = authenticationTechnique.RollYourOwn;
+                debugOutput("AuthTechnique: Roll Your Own");
+                debugOutput("AuthType: Basic");
+            }
+            else
+            {
+                rClient.authTech = authenticationTechnique.NetworkCredential;
+                debugOutput("AuthTechnique: NetworkCredential");
+                debugOutput("AuthType:??? NetCred decides!");
+            }
+            
+            rClient.userName = txtUserName.Text;
+            rClient.userPassword = txtPassword.Text;
+            
+            debugOutput("Rest Client Created");
+            
+            string strResponse = string.Empty;
+
+            strResponse = rClient.makeRequest();
+
+            debugOutput(strResponse);
+
+
+        }
+
+        #endregion
+
+        private void debugOutput(string strDebugText)
+        {
+            try
+            {
+                System.Diagnostics.Debug.Write(strDebugText+Environment.NewLine);
+                txtResponse.Text = txtResponse.Text + strDebugText + Environment.NewLine;
+                txtResponse.SelectionStart = txtResponse.TextLength;
+                txtResponse.ScrollToCaret();
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.Write(ex.Message,ToString()+Environment.NewLine);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frm2 frm2 = new frm2(this);
+            frm2.Show();
+        }
+    }
+}
